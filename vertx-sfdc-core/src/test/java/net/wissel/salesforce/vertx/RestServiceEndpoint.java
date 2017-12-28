704387aede69277becebcfc599b9c76755640876
Stephan H. Wissel <stw@linux.com>
2017-12-28 12:21:30 +0800

    Started REST endpoint for testing

---
+++ vertx-sfdc-core/src/test/java/net/wissel/salesforce/vertx/RestServiceEndpoint.java
@@ -0,0 +1,34 @@
+/** ========================================================================= *
+ * Copyright (C)  2017, 2018 Salesforce Inc ( http://www.salesforce.com/      *
+ *                            All rights reserved.                            *
+ *                                                                            *
+ *  @author     Stephan H. Wissel (stw) <swissel@salesforce.com>              *
+ *                                       @notessensei                         *
+ * @version     1.0                                                           *
+ * ========================================================================== *
+ *                                                                            *
+ * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
+ * not use this file except in compliance with the License.  You may obtain a *
+ * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
+ *                                                                            *
+ * Unless  required  by applicable  law or  agreed  to  in writing,  software *
+ * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
+ * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
+ * License for the  specific language  governing permissions  and limitations *
+ * under the License.                                                         *
+ *                                                                            *
+ * ========================================================================== *
+ */
+package net.wissel.salesforce.vertx;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Echo server that simply answers with randomly 200, 400 or 500
+ * and proper results - needed for testing the interface verticles
+ * @author swissel
+ *
+ */
+public class RestServiceEndpoint extends AbstractVerticle {
+
+}
