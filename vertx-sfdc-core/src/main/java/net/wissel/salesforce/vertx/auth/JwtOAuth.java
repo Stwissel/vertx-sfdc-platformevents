0072ea2e9e2e4fbb8693842be47b133dc2ad8cf4
Stephan H. Wissel <stw@linux.com>
2018-06-14 13:40:22 +1000

    Empty JWT class (WIP)

---
+++ vertx-sfdc-core/src/main/java/net/wissel/salesforce/vertx/auth/JwtOAuth.java
@@ -0,0 +1,41 @@
+/** ========================================================================= *
+ * Copyright (C)  2017, 2018 Salesforce Inc ( http://www.salesforce.com/      *
+ *                            All rights reserved.                            *
+ *                                                                            *
+ *  @author     Stephan H. Wissel (stw) <swissel@salesforce.com>              *
+ *                                       @notessensei                         *
+ * @version     1.0                                                           *
+ * ========================================================================== *
+ *                                                                            *
+ * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
+ * not use this file except in compliance with the License.  You may obtain a *
+ * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
+ *                                                                            *
+ * Unless  required  by applicable  law or  agreed  to  in writing,  software *
+ * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
+ * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
+ * License for the  specific language  governing permissions  and limitations *
+ * under the License.                                                         *
+ *                                                                            *
+ * ========================================================================== *
+ */
+package net.wissel.salesforce.vertx.auth;
+
+import io.vertx.core.Future;
+
+/**
+ * @author swissel
+ *
+ */
+public class JwtOAuth extends AbstractAuth {
+
+    /**
+     * @see net.wissel.salesforce.vertx.auth.AbstractAuth#login(io.vertx.core.Future)
+     */
+    @Override
+    protected void login(Future<AuthInfo> futureAuthinfo) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
